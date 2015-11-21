b5bfb84208c77181f990cb1d2d398343ec0f316f
Janik Knittle <janik.knittle@gmail.com>
2015-11-21 00:05:36 -0800

    Added Username to Users and allowed to use for Sign In

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :vehicles
-  
+  validate :validate_username
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120231800) do
+ActiveRecord::Schema.define(version: 20151121075042) do
 
   create_table "service_fields", force: :cascade do |t|
     t.string   "name"
@@ -60,11 +60,13 @@ ActiveRecord::Schema.define(version: 20151120231800) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "vehicles", force: :cascade do |t|
     t.integer  "year"
